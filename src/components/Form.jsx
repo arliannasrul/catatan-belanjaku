95f1707ce72631fc1fdd46e292670fd8ee2e907f
arlianansrul <arliannasrul@gmail.com>
2025-02-25 09:49:07 +0700

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-export default function Form({onAddItem}) {    // Form component merupakan komponen yang menampilkan form untuk menambahkan item belanjaan
+export default function Form({ onAddItem }) {    // Form component merupakan komponen yang menampilkan form untuk menambahkan item belanjaan
   const [name, setName] = useState('');
   const [amount, setAmount] = useState(1);
   const [unit, setUnit] = useState('kg');
-  function handleSubmit(e) {  
+  function handleSubmit(e) {
     e.preventDefault();
 
-    if(!name) return;   // Jika input nama barang kosong, maka hentikan eksekusi kode selanjutnya
-    
+    if (!name) return;   // Jika input nama barang kosong, maka hentikan eksekusi kode selanjutnya
+
     const newItem = {
       id: Date.now(),
       name,
@@ -17,7 +17,7 @@ export default function Form({onAddItem}) {    // Form component merupakan kompo
       checked: false,
     };
     onAddItem(newItem);
-    console.log(newItem); 
+    console.log(newItem);
 
     setName('');    // Mengosongkan form setelah submit
     setAmount(1);
@@ -27,17 +27,17 @@ export default function Form({onAddItem}) {    // Form component merupakan kompo
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div className="form-group">
-        <input className= "jumlah" type="number" min="1" placeholder="Jumlah" value={amount} onChange={(e) => setAmount(Number(e.target.value))}/>
+        <input className="jumlah" type="number" min="1" placeholder="Jumlah" value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
         <select className="satuan" placeholder="Satuan" value={unit} onChange={(e) => setUnit(e.target.value)}>
-            <option value="kg">kg</option>
-            <option value="gram">gram</option>
-            <option value="liter">liter</option>
-            <option value="meter">meter</option>
-            <option value="pcs">biji</option>
-          </select>
-        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)}/>
+          <option value="kg">kg</option>
+          <option value="gram">gram</option>
+          <option value="liter">liter</option>
+          <option value="meter">meter</option>
+          <option value="pcs">biji</option>
+        </select>
+        <input className="nama-brg" type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
       </div>
-      <button>Tambah</button>    
+      <button>Tambah</button>
     </form>
   );
 }
